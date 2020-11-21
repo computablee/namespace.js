59225be20d32c91543a02d26b1ef90bd86072ea9
Lane <iamaperson620@gmail.com>
2020-11-21 00:59:22 -0600

    added example files

---
+++ example/source.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+
+
+namespace test
+{
+	struct mystruct
+	{
+		int a;
+		int b;
+	};
+	
+	int thisa;
+	
+	namespace nested_test
+	{
+		struct test::mystruct add(int a, int b)
+		{
+			return (struct test::mystruct) { .a = a, .b = test::thisa };
+		}
+		
+		struct test::mystruct (*testfunc(void))(int, int)
+		{
+			return test::nested_test::add;
+		}
+		
+		void printa(struct test::mystruct mystr)
+		{
+			const char *str = "%d\n";
+			int x = 5;
+			printf(str, mystr.a);
+		}
+	}
+	
+	int zero()
+	{
+		return 0;
+	}
+}
+
+int main()
+{
+	test::thisa = 7;
+	test::nested_test::printa(test::nested_test::testfunc()(5, 6));
+	printf("%d\n", test::nested_test::testfunc()(5, 6).b);
+	return test::zero();
+}
